f63523021a4a5caa7697c200d6c8a8e37941ecb1
fahimdipto <fahimdipto>
2020-11-08 11:44:20 +0000

    remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6xl7s+k7z99fb*ae79d$zm*w%j^ylzism7@&h!oi=2+id61yot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tfangp0wer.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= BASE_DIR / 'static'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
